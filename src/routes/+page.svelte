985dbedb762d33dd33599b8ac28c820b8b3fcaa9
Dorian Zedler <dorian@itsblue.de>
2023-11-18 22:22:14 +0100

    Fix: minor issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Chat from './Chat.svelte';
-	import ChatSidebar from './ChatSidebar.svelte';
 </script>
 
 <div class="container h-full w-full overflow-hidden mx-auto max-h-full flex flex-col-reverse">
