57049589e4b2dc5728c611835123543a50ac8f78
timkn <timknothe21@gmail.com>
2023-11-18 13:57:47 +0100

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,11 @@
 </script>
 
 <div class="p-8">
+	<P class="mb-4"
+		>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia quibusdam aliquid in vitae velit
+		tempora harum illum itaque! Assumenda dolorem ratione explicabo optio blanditiis sit, ex
+		exercitationem voluptates deleniti sequi?</P
+	>
 	<div class="grid grid-cols-2 gap-4">
 		<Faq />
 		<Chat />
