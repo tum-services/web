bf533bcc9c0e261e851ba5265a2b5aa41c971a23
Dorian Zedler <dorian@itsblue.de>
2023-11-18 03:50:04 +0100

    Feat: chat

--- src/routes/Chat.svelte
+++ src/routes/Chat.svelte
@@ -1,47 +1,71 @@
 <script lang="ts">
-	let messages: {
-		author: 'bot' | 'user';
-		message: string;
-	}[] = [
+	import { Card, Label, Textarea } from 'flowbite-svelte';
+	import { LoremIpsum } from 'lorem-ipsum';
+	import {
+		GiftBoxSolid,
+		ArrowUpRightFromSquareOutline,
+		UserSolid,
+		HeadphonesSolid
+	} from 'flowbite-svelte-icons';
+	import type { Message } from '$lib/MessageBox.svelte';
+	import MessageBox from '$lib/MessageBox.svelte';
+
+	const lorem = new LoremIpsum({
+		sentencesPerParagraph: {
+			max: 8,
+			min: 4
+		},
+		wordsPerSentence: {
+			max: 16,
+			min: 4
+		}
+	});
+
+	let messages: Message[] = [
 		{
 			author: 'user',
-			message: 'Hello'
+			message: lorem.generateSentences(1)
 		},
 		{
 			author: 'bot',
-			message: 'Hi, how are you?'
+			message: lorem.generateSentences(3)
 		},
 		{
 			author: 'user',
-			message: "I'm fine, thanks"
+			message: lorem.generateSentences(5)
 		},
 		{
 			author: 'bot',
-			message: 'Good to hear that'
+			message: lorem.generateSentences(12)
 		}
 	];
+
+	const onKeyPress = (event: KeyboardEvent) => {
+		const target = event.target as HTMLTextAreaElement;
+
+		if (event.key === 'Enter' && !event.shiftKey) {
+			if (target.value.trim() !== '') {
+				messages = [
+					...messages,
+					{
+						author: 'user',
+						message: target.value
+					}
+				];
+			}
+			target.value = '';
+			event.preventDefault();
+		}
+	};
 </script>
 
-<div class="flex flex-col">
-	{#each messages as message}
-		{#if message.author === 'bot'}
-			<div class="flex flex-row justify-start">
-				<div class="flex">
-					{message.message}
-				</div>
-			</div>
-		{:else}
-			<div class="flex flex-row justify-end">
-				<div class="flex">
-					{message.message}
-				</div>
-			</div>
-		{/if}
-	{/each}
-</div>
-
-<div class="flex flex-row justify-center">
-	<div class="flex">
-		<input type="text" placeholder="Type your message here..." />
+<div class="flex flex-col gap-3">
+	<div class="flex flex-col-reverse w-full gap-3 overflow-scroll h-[80vh]">
+		{#each messages.slice().reverse() as message}
+			<MessageBox {message} />
+		{/each}
 	</div>
+
+	<Label for="textarea-id" class="mb-2">Your message</Label>
+	<Textarea id="textarea-id" on:keypress={onKeyPress} placeholder="Type your message here..." />
 </div>
