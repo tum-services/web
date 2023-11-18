6a427cb317ea4d417b73ce50e1d3ae9d1fe45abe
timkn <timknothe21@gmail.com>
2023-11-18 03:02:59 +0100

    lul

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script>
 	import Faq from '$lib/FAQ.svelte';
 	import { Alert, P } from 'flowbite-svelte';
+	import Chat from './Chat.svelte';
 </script>
 
 <div class="p-8">
 	<div class="flex justify-stretch justify-items-stretch">
 		<Faq />
-		<div>
-			<P>Chat!</P>
-		</div>
+		<Chat />
 	</div>
 </div>
