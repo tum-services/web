b5af12419f9abebabbb4a5a668356aff793068a1
Dorian Zedler <dorian@itsblue.de>
2023-11-18 22:19:04 +0100

    Chore: remove sidebar

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,21 +1,9 @@
 <script>
-    import {Button, DarkMode, Heading, Mark, Secondary} from 'flowbite-svelte';
-    import {BarsOutline} from "flowbite-svelte-icons";
-    export let sidebarHidden;
+	import { DarkMode, Heading, Mark, Secondary } from 'flowbite-svelte';
 </script>
 
 <nav class="flex justify-between p-8 w-full bg-slate-200 dark:bg-slate-900">
-
-    <div class="mr-4 md:hidden">
-        <Button outline={true}
-                on:click={() => {
-				sidebarHidden = false;
-			}}><BarsOutline/></Button
-        >
-
-    </div>
-
-	<Heading tag="h1"
+	<Heading tag="h2"
 		><Mark>TUM</Mark>.services <Secondary class="ml-2 text-sm"
 			>where Information Finds You!</Secondary
 		></Heading
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,13 @@
 <script>
-    import {CloseButton, Drawer} from 'flowbite-svelte';
 	import Header from '$lib/Header.svelte';
 	import '../app.postcss';
-    import {sineIn} from "svelte/easing";
-    import ChatSidebar from "./ChatSidebar.svelte";
-
-    let sidebarHidden = true;
-
-
-    let transitionParams = {
-        x: -320,
-        duration: 200,
-        easing: sineIn
-    };
 </script>
 
 <div
 	class="h-[100vh] w-[100vw] overflow-hidden flex flex-col justify-items-stretch items-stretch justify-stretch"
 >
-	<Header bind:sidebarHidden={sidebarHidden} />
+	<Header />
 	<div class="relative h-full max-h-full w-full p-[1rem] flex-1 overflow-hidden">
-		<slot sidebarHidden={sidebarHidden} />
+		<slot />
 	</div>
 </div>
-
-
-<Drawer {transitionParams} transitionType="fly" bind:hidden={sidebarHidden}>
-    <div class="flex items-center">
-        <CloseButton on:click={() => (sidebarHidden = true)} class="mb-4 dark:text-white lg:hidden"/>
-    </div>
-    <ChatSidebar/>
-</Drawer>
