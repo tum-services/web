0e4042019b004994ee7791b7969f909b615772c0
timkn <timknothe21@gmail.com>
2023-11-18 02:20:25 +0100

    basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import Footer from '$lib/Footer.svelte';
+	import Header from '$lib/Header.svelte';
+	import '../app.postcss';
+</script>
+
+<Header />
+<slot />
+<Footer />
