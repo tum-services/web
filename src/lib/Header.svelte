0b9ce9663ba962e95fa5ff6f8a0f85f804ecfc09
timkn <timknothe21@gmail.com>
2023-11-18 02:32:29 +0100

    added basic header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { DarkMode, Heading, Mark, Secondary } from 'flowbite-svelte';
+</script>
+
+<nav class="flex justify-between m-8">
+	<Heading tag="h1"
+		><Mark>TUM</Mark>.services <Secondary class="ml-2">where Information Finds You!</Secondary
+		></Heading
+	>
+	<DarkMode />
+</nav>
